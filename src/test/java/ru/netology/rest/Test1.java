6668d250aac0e62733e106c02f0633b85d635822
Kate Popova <popova1919@yandex.ru>
2025-08-02 19:51:06 +0530

    Test1 failed

--- src/test/java/ru/netology/rest/Test1.java
+++ src/test/java/ru/netology/rest/Test1.java
@@ -13,7 +13,7 @@ public class Test1 {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("sim sim otkroysa") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
